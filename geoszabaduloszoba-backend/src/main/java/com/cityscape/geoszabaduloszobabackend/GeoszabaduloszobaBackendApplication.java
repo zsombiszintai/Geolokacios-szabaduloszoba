bcaffb955cd66ee6a7bb4d3ae4da8f8854a48cac
zsombiszintai <zsombiszintai@gmail.com>
2026-02-23 17:52:23 +0100

    Projekt letrehozas

---
+++ geoszabaduloszoba-backend/src/main/java/com/cityscape/geoszabaduloszobabackend/GeoszabaduloszobaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cityscape.geoszabaduloszobabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoszabaduloszobaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoszabaduloszobaBackendApplication.class, args);
+    }
+
+}
---
+++ geoszabaduloszoba-backend/src/test/java/com/cityscape/geoszabaduloszobabackend/GeoszabaduloszobaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityscape.geoszabaduloszobabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoszabaduloszobaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

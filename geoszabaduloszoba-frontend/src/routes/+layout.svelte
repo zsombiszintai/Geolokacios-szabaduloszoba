b68a8087757abd1bfe56dd7adb02d82ef16bc552
zsombiszintai <zsombiszintai@gmail.com>
2026-03-05 13:41:13 +0100

    Tailwind CSS, Flowbite Svelte

--- geoszabaduloszoba-frontend/src/routes/+layout.svelte
+++ geoszabaduloszoba-frontend/src/routes/+layout.svelte
@@ -1,11 +1,10 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+<script>import "../app.css";
+import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
-</script>
+let { children } = $props();</script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="{favicon}" />
 </svelte:head>
 
 {@render children()}

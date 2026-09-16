7fb2d499880659f21edf19a72bc04e73aa1acb50
zsombiszintai <zsombiszintai@gmail.com>
2026-09-16 23:18:45 +0200

    Add Cityscape Keycloak login theme

---
+++ cityscape-keycloak-theme/src/login/KcPage.tsx
@@ -0,0 +1,47 @@
+import "@fontsource/josefin-sans/latin-400.css";
+import "@fontsource/josefin-sans/latin-600.css";
+import "@fontsource/josefin-sans/latin-700.css";
+import "@fontsource/josefin-sans/latin-ext-400.css";
+import "@fontsource/josefin-sans/latin-ext-600.css";
+import "@fontsource/josefin-sans/latin-ext-700.css";
+import "./cityscape.css";
+import { Suspense, lazy } from "react";
+import type { ClassKey } from "keycloakify/login";
+import type { KcContext } from "./KcContext";
+import { useI18n } from "./i18n";
+import DefaultPage from "keycloakify/login/DefaultPage";
+import Template from "keycloakify/login/Template";
+const UserProfileFormFields = lazy(
+    () => import("keycloakify/login/UserProfileFormFields")
+);
+
+const doMakeUserConfirmPassword = true;
+
+export default function KcPage(props: { kcContext: KcContext }) {
+    const { kcContext } = props;
+
+    const { i18n } = useI18n({ kcContext });
+
+    return (
+        <Suspense>
+            {(() => {
+                switch (kcContext.pageId) {
+                    default:
+                        return (
+                            <DefaultPage
+                                kcContext={kcContext}
+                                i18n={i18n}
+                                classes={classes}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                                UserProfileFormFields={UserProfileFormFields}
+                                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                            />
+                        );
+                }
+            })()}
+        </Suspense>
+    );
+}
+
+const classes = {} satisfies { [key in ClassKey]?: string };
